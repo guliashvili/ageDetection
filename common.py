ee519ebcd8f24105f1e0a0b1769b4112f5f757e7
George Guliashvili <gguli@fb.com>
2018-05-28 15:42:42 +0100

    get rid of generator

--- common.py
+++ common.py
@@ -3,6 +3,6 @@ import os
 def get_images(dir):
     dir = os.path.expanduser(dir)
     files = (os.path.join(dir, file) for file in os.listdir(dir))
-    files = (file for file in files if os.path.isfile(file) and not file.endswith('.txt') and '.' in file)
+    files = [file for file in files if os.path.isfile(file) and not file.endswith('.txt') and '.' in file]
 
     return files
