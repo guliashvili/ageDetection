2f2792b22bbc8ab051191c79b687753f2397f3b6
George Guliashvili <gguli@fb.com>
2018-05-28 15:59:03 +0100

    added photos for testing. if photo contains more than 1 face or 0, it will not be saved in out folder

--- align/mtcnn_align.py
+++ align/mtcnn_align.py
@@ -94,18 +94,18 @@ def main(args):
                     bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold,
                                                                 factor)
                     nrof_faces = bounding_boxes.shape[0]
-                    if nrof_faces > 0:
+                    if nrof_faces == 1:
                         det = bounding_boxes[:, 0:4]
                         img_size = np.asarray(img.shape)[0:2]
-                        if nrof_faces > 1:
-                            bounding_box_size = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
-                            img_center = img_size / 2
-                            offsets = np.vstack([(det[:, 0] + det[:, 2]) / 2 - img_center[1],
-                                                 (det[:, 1] + det[:, 3]) / 2 - img_center[0]])
-                            offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
-                            index = np.argmax(
-                                bounding_box_size - offset_dist_squared * 2.0)  # some extra weight on the centering
-                            det = det[index, :]
+                        # if nrof_faces > 1:
+                        #     bounding_box_size = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
+                        #     img_center = img_size / 2
+                        #     offsets = np.vstack([(det[:, 0] + det[:, 2]) / 2 - img_center[1],
+                        #                          (det[:, 1] + det[:, 3]) / 2 - img_center[0]])
+                        #     offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
+                        #     index = np.argmax(
+                        #         bounding_box_size - offset_dist_squared * 2.0)  # some extra weight on the centering
+                        #     det = det[index, :]
                         det = np.squeeze(det)
                         bb = np.zeros(4, dtype=np.int32)
                         bb[0] = np.maximum(det[0] - args.margin / 2, 0)
@@ -117,12 +117,8 @@ def main(args):
                         nrof_successfully_aligned += 1
                         misc.imsave(output_filename, scaled)
                         text_file.write('%s %d %d %d %d\n' % (output_filename, bb[0], bb[1], bb[2], bb[3]))
-                    else:
-                        print('Unable to align "%s"' % image_path)
-                        text_file.write('%s\n' % (output_filename))
 
-    print('Total number of images: %d' % nrof_images_total)
-    print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
+    print('Aligned %d/%d' % (nrof_images_total, nrof_successfully_aligned))
 
 
 def parse_arguments(argv):
