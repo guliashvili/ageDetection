4dddc18e20c4d9babc2cb15a8ac84ee5becc1fa6
George Guliashvili <gguli@fb.com>
2018-05-28 21:55:46 +0100

    padding

--- align/mtcnn_align.py
+++ align/mtcnn_align.py
@@ -24,8 +24,9 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import os,sys,inspect
-sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
+import os, sys, inspect
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
 from scipy import misc
 import argparse
 import tensorflow as tf
@@ -35,6 +36,39 @@ import random
 from time import sleep
 import common
 import math
+
+
+def pad(img, margin):
+    margin = int(margin / 2) + 1
+    return np.pad(img, ((margin, margin), (margin, margin), (0, 0)), 'constant'), margin
+
+
+def align(img):
+    imgSize = (112, 96)
+
+    x_ = [30.2946, 65.5318, 48.0252, 33.5493, 62.7299]
+    y_ = [51.6963, 51.5014, 71.7366, 92.3655, 92.2041]
+
+    src = np.array(zip(x_, y_)).astype(np.float32).reshape(1, 5, 2)
+
+    alignedFaces = []
+
+    # there might be more than one faces, hence
+    # multiple sets of points
+    for pset in points:
+        img2 = img.copy()
+
+        pset_x = pset[0:5]
+        pset_y = pset[5:10]
+
+        dst = np.array(zip(pset_x, pset_y)).astype(np.float32).reshape(1, 5, 2)
+
+        transmat = cv2.estimateRigidTransform(dst, src, False)
+
+        out = cv2.warpAffine(img2, transmat, (imgSize[1], imgSize[0]))
+
+        alignedFaces.append(out)
+
 
 def to_rgb(img):
     w, h = img.shape
@@ -64,64 +98,61 @@ def main(args):
     threshold = [0.6, 0.7, 0.7]  # three steps's threshold
     factor = 0.709  # scale factor
 
-    bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes.txt')
+    nrof_images_total = 0
+    nrof_successfully_aligned = 0
+    for image_path in dataset:
+        nrof_images_total += 1
+        filename = os.path.splitext(os.path.split(image_path)[1])[0]
+        output_filename = os.path.join(output_dir, filename + '.png')
+        if math.floor(100.0 * nrof_images_total / len(dataset)) != math.floor(
+                100.0 * (nrof_images_total - 1) / len(dataset)):
+            print(math.floor(100.0 * nrof_images_total / len(dataset)), '%')
 
-    with open(bounding_boxes_filename, "w") as text_file:
-        nrof_images_total = 0
-        nrof_successfully_aligned = 0
-        for image_path in dataset:
-            nrof_images_total += 1
-            filename = os.path.splitext(os.path.split(image_path)[1])[0]
-            output_filename = os.path.join(output_dir, filename + '.png')
-            if math.floor(100.0 * nrof_images_total / len(dataset)) != math.floor(100.0 * (nrof_images_total - 1) / len(dataset)):
-                print(math.floor(100.0 * nrof_images_total / len(dataset)), '%')
-            if not os.path.exists(output_filename):
-                try:
-                    img = misc.imread(image_path)
-                except (IOError, ValueError, IndexError) as e:
-                    errorMessage = '{}: {}'.format(image_path, e)
-                    print(errorMessage)
-                else:
-                    if img.ndim < 2:
-                        print('Unable to align "%s"' % image_path)
-                        #text_file.write('%s\n' % (output_filename))
-                        continue
-                    if img.ndim == 2:
-                        img = to_rgb(img)
-                    img = img[:, :, 0:3]
-                    print('shape', img.shape)
-                    print('img', img)
+        if not os.path.exists(output_filename):
+            try:
+                img = misc.imread(image_path)
+            except (IOError, ValueError, IndexError) as e:
+                errorMessage = '{}: {}'.format(image_path, e)
+                print(errorMessage)
+            else:
+                if img.ndim < 2:
+                    print('Unable to align "%s"' % image_path)
+                    # text_file.write('%s\n' % (output_filename))
+                    continue
+                if img.ndim == 2:
+                    img = to_rgb(img)
+                img = img[:, :, 0:3]
+                print('shape', img.shape)
+                print('img', img)
 
-                    bounding_boxes, points = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold,
-                                                                factor)
-                    print(image_path, '\n', bounding_boxes, '\n', points)
-                    nrof_faces = bounding_boxes.shape[0]
-                    if nrof_faces == 1:
-                        points = points[0]
-                        det = bounding_boxes[:, 0:4]
-                        img_size = np.asarray(img.shape)[0:2]
-                        # if nrof_faces > 1:
-                        #     bounding_box_size = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
-                        #     img_center = img_size / 2
-                        #     offsets = np.vstack([(det[:, 0] + det[:, 2]) / 2 - img_center[1],
-                        #                          (det[:, 1] + det[:, 3]) / 2 - img_center[0]])
-                        #     offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
-                        #     index = np.argmax(
-                        #         bounding_box_size - offset_dist_squared * 2.0)  # some extra weight on the centering
-                        #     det = det[index, :]
-                        det = np.squeeze(det)
-                        bb = np.zeros(4, dtype=np.int32)
-                        bb[0] = np.maximum(det[0] - args.margin / 2, 0)
-                        bb[1] = np.maximum(det[1] - args.margin / 2, 0)
-                        bb[2] = np.minimum(det[2] + args.margin / 2, img_size[1])
-                        bb[3] = np.minimum(det[3] + args.margin / 2, img_size[0])
-                        cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
-                        scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
-                        nrof_successfully_aligned += 1
-                        misc.imsave(output_filename, scaled)
-                        text_file.write('%s %d %d %d %d\n' % (output_filename, bb[0], bb[1], bb[2], bb[3]))
+                bounding_boxes, points = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold,
+                                                                 factor)
+                print(image_path, '\n', bounding_boxes, '\n', points)
+                nrof_faces = bounding_boxes.shape[0]
+                if nrof_faces == 1:
+                    img, pad = pad(img, margin)
 
-    print('Aligned %d/%d' % (nrof_successfully_aligned, nrof_images_total))
+                    points = points[0]
+                    det = bounding_boxes[:, 0:4]
+
+                    points += pad
+                    det += pad
+
+
+                    img_size = np.asarray(img.shape)[0:2]
+                    det = np.squeeze(det)
+                    bb = np.zeros(4, dtype=np.int32)
+                    bb[0] = det[0] - args.margin / 2
+                    bb[1] = det[1] - args.margin / 2
+                    bb[2] = det[2] + args.margin / 2
+                    bb[3] = det[3] + args.margin / 2
+                    cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+                    scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
+                    nrof_successfully_aligned += 1
+                    misc.imsave(output_filename, scaled)
+
+
+print('Aligned %d/%d' % (nrof_successfully_aligned, nrof_images_total))
 
 
 def parse_arguments(argv):
