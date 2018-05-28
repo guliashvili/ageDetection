234b35714787383989f976d538e9ded04e39afeb
George Guliashvili <gguli@fb.com>
2018-05-28 15:34:14 +0100

    list dir python 2 compatible

--- common.py
+++ common.py
@@ -2,7 +2,7 @@ import os
 
 def get_images(dir):
     dir = os.path.expanduser(dir)
-    files = (os.path.join(dir, file) for file in os.path.listdir(dir))
+    files = (os.path.join(dir, file) for file in os.listdir(dir))
     files = (file for file in files if os.path.isfile(file) and not file.endswith('.txt') and '.' in file)
 
     return files
