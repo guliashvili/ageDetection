6aa502cb99deb246e8e330eb519bc1c1bf0f8f97
George Guliashvili <gguli@fb.com>
2018-06-28 01:16:18 +0100

    fix

--- imdbcrawl/crawl.py
+++ imdbcrawl/crawl.py
@@ -4,11 +4,11 @@ from multiprocessing import Pool
 
 def count(s, w):
     c = 0
-    for i in range(0, len(s) - len(w)):
+    for i in range(0, len(s) - len(w) - 1):
         if s[i].isalpha() or s[i + len(w)].isalpha():
             continue
         else:
-            if s[i:i + len(w)] == w:
+            if s[i + 1:i + len(w) + 1] == w:
                 c += 1
     return c
 
