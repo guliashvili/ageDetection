d659e6b24090e3e978c7de3c3a0d07e74de033c4
George Guliashvili <gguli@fb.com>
2018-06-29 01:17:44 +0100

    executing whole imdb dumper catch exceptions

--- imdbcrawl/crawl.py
+++ imdbcrawl/crawl.py
@@ -188,7 +188,7 @@ def main():
     newlines = [(x.split()[0], int(x.split()[1])) for x in list(filter(None, open('strip.name', 'r')
                 .read().split('\n')))]
 
-    cpus = multiprocessing.cpu_count()*5
+    cpus = multiprocessing.cpu_count()*10
     pool = multiprocessing.Pool(processes=cpus)
     data = pool.map(doit, newlines[:1000])
 
