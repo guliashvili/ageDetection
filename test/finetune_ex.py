ca82f2b4e79b53a42dafb17004f0e954d8793ecc
George Guliashvili <gguli@fb.com>
2018-07-04 02:36:30 +0100

    almost generic trainer

--- test/finetune_ex.py
+++ test/finetune_ex.py
@@ -1,27 +1,16 @@
 import os, sys
 import argparse
 import mxnet as mx
+import logging
+head = '%(asctime)-15s %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=head)
 
 def main(args):
-    if sys.version_info[0] >= 3:
-        from urllib.request import urlretrieve
-    else:
-        from urllib import urlretrieve
-
-    def download(url):
-        filename = url.split("/")[-1]
-        print(filename)
-        if not os.path.exists(filename):
-            urlretrieve(url, filename)
-
-
-
-
     def get_iterators(batch_size, data_shape=(3, 224, 224)):
         train = mx.io.ImageRecordIter(
             path_imgrec         = args.lst + 'train.rec',
             data_name           = 'data',
-            label_name          = 'softmax_label',
+            label_name          = 'maintheend_label,
             batch_size          = batch_size,
             data_shape          = data_shape,
             shuffle             = True,
@@ -31,7 +20,7 @@ def main(args):
         val = mx.io.ImageRecordIter(
             path_imgrec         = args.lst + 'valid.rec',
             data_name           = 'data',
-            label_name          = 'softmax_label',
+            label_name          = 'maintheend_label',
             batch_size          = batch_size,
             data_shape          = data_shape,
             rand_crop           = False,
@@ -40,7 +29,7 @@ def main(args):
         test = mx.io.ImageRecordIter(
             path_imgrec         = args.lst + 'test.rec',
             data_name           = 'data',
-            label_name          = 'softmax_label',
+            label_name          = 'maintheend_label',
             batch_size          = batch_size,
             data_shape          = data_shape,
             rand_crop           = False,
@@ -49,47 +38,21 @@ def main(args):
         return (train, val, test)
 
 
-    def get_model(prefix, epoch):
-        download(prefix+'-symbol.json')
-        download(prefix+'-%04d.params' % (epoch,))
+    sym, arg_params, aux_params = mx.model.load_checkpoint(args.prefix, args.epochsave)
 
-    get_model('http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50', 0)
-    sym, arg_params, aux_params = mx.model.load_checkpoint('resnet-50', 0)
-
-    def get_fine_tune_model(symbol, arg_params, num_classes, layer_name='flatten0'):
-        """
-        symbol: the pretrained network symbol
-        arg_params: the argument parameters of the pretrained model
-        num_classes: the number of classes for the fine-tune datasets
-        layer_name: the layer name before the last fully-connected layer
-        """
-        all_layers = symbol.get_internals()
-        net = all_layers[layer_name+'_output']
-        net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
-        net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
-        new_args = dict({k:arg_params[k] for k in arg_params if 'fc1' not in k})
-        return (net, new_args)
-
-
-    import logging
-    head = '%(asctime)-15s %(message)s'
-    logging.basicConfig(level=logging.DEBUG, format=head)
 
     def fit(symbol, arg_params, aux_params, train, val, test, batch_size, num_gpus):
         devs = [mx.gpu(i) for i in range(num_gpus)]
         mod = mx.mod.Module(symbol=symbol, context=devs)
         eval = None
-        if args.accu == 'acc':
-            eval = mx.metric.Accuracy()
-        elif args.accu[0] == 'k':
-            eval = mx.metric.TopKAccuracy(int(args.accu[1:]))
+        eval = mx.metric.Accuracy()
         mod.fit(train, val,
             num_epoch=args.epoch,
             arg_params=arg_params,
             aux_params=aux_params,
             allow_missing=True,
             batch_end_callback = mx.callback.Speedometer(batch_size, 10),
-            epoch_end_callback = mx.callback.do_checkpoint(args.name, 1),
+            epoch_end_callback = mx.callback.do_checkpoint(args.prefix, 1),
             kvstore='device',
             optimizer='sgd',
             optimizer_params={'learning_rate':0.01},
@@ -98,24 +61,20 @@ def main(args):
         metric = mx.metric.Accuracy()
         return mod.score(test, metric)
 
-    num_classes = args.num
     batch_per_gpu = 64
     num_gpus = args.num_gpus
 
-    (new_sym, new_args) = get_fine_tune_model(sym, arg_params, num_classes)
-
     batch_size = batch_per_gpu * num_gpus
     (train, val, test) = get_iterators(batch_size)
-    mod_score = fit(new_sym, new_args, aux_params, train, val, test, batch_size, num_gpus)
+    mod_score = fit(sym, arg_params, aux_params, train, val, test, batch_size, num_gpus)
     print(mod_score)
 
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('name', type=str, help='Name of training')
-    parser.add_argument('num', type=str, help='num classes')
-    parser.add_argument('accu', type=str, help='error function')
+    parser.add_argument('prefix', type=str, help='prefix')
+    parser.add_argument('epochsave', type=int, help='epoch to start from')
     parser.add_argument('epoch', type=int, help='num of epochs')
     parser.add_argument('lst', type=str, help='Train lst')
     parser.add_argument('num_gpus', type=int,
