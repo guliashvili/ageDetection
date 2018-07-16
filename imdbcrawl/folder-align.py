34eb9e6b513e769ea20d8fe3e39881d48bfd4b46
George Guliashvili <gguli@fb.com>
2018-07-16 19:40:00 +0100

    folder align only jpg

--- imdbcrawl/folder-align.py
+++ imdbcrawl/folder-align.py
@@ -25,15 +25,7 @@ import random
 def get_images(dir):
     dir = os.path.expanduser(dir)
     files = (os.path.join(dir, file) for file in os.listdir(dir))
-    files = [file for file in files if os.path.isfile(file)
-            and not file.endswith('.txt')
-            and '.' in file
-            and not file.endswith('.DS_Store')
-            and not file.endswith('.csv')
-            and not file.endswith('.lst')
-            and not file.endswith('.bin')
-            and not file.endswith('.idx')
-            and not file.endswith('.rec')]
+    files = [file for file in files if os.path.isfile(file) and file.lower().endswith('.jpg')]
 
     return files
 CPU = 1
